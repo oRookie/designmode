da47703d68cd66d724941bd4426b606289e9195d
like <like@qiban.com>
2019-04-01 22:37:40 +0800

    factory

--- src/main/java/factory/ShapeFactory.java
+++ src/main/java/factory/ShapeFactory.java
@@ -1,4 +1,11 @@
 package factory;
 
 public class ShapeFactory {
+    public static Shape getShape(String type){
+        if (type.equalsIgnoreCase("RECTANGLE")) return new Rectangle();
+        else if (type.equalsIgnoreCase("SQUARE")) return new Square();
+        else if (type.equalsIgnoreCase("CIRCLE")) return new Circle();
+        return  null;
+    };
+
 }
